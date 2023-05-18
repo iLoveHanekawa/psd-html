ac83d1f2e5f7da6c5722a2d7dda2338b53d23fe6
Arjun Tanwar <arjunthakur900@gmail.com>
2023-05-18 13:40:15 +0530

    added

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
 // scrolling
 
 const container = document.querySelector(".footer-images-container");
-
-const horizontalScroll = () => {
-    container.scrollLeft += 2;
-}
-let scrollId = null;
-
-const resumeScroll = () => {
-    scrollId = setInterval(horizontalScroll, 20);
-}
-container.addEventListener('mouseleave', function () {
-    clearInterval(scrollId);
-});
-
-container.addEventListener('mouseenter', resumeScroll);
-
 container.addEventListener('scroll', function() {
+    console.log(container.scrollLeft);
     if(container.scrollLeft + container.clientWidth + 3 >= container.scrollWidth) {
         var firstImage = container.querySelector(".footer-image-link");
         container.appendChild(firstImage.cloneNode(true));
