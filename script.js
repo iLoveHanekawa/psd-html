dcd19ec8877fd4078a6045f5d4e4ba678ac9e187
Arjun Tanwar <arjunthakur900@gmail.com>
2023-05-18 10:59:29 +0530

    added

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// scrolling
+
 const container = document.querySelector(".footer-images-container");
 
 const horizontalScroll = () => {
@@ -22,4 +24,23 @@ container.addEventListener('scroll', function() {
         container.removeChild(firstImage);
     }
 });
-console.log(container);
+
+//accordion
+
+const pageItems = document.querySelectorAll(".footer-pages-item");
+pageItems.forEach(item => {
+    let button = item.querySelector('.footer-pages-button');
+    button.addEventListener("click", () => {
+        let nav = item.querySelector('.footer-pages-nav');
+        if(nav.clientHeight === 0) {
+            nav.style.maxHeight = "108.3px";
+            nav.style.padding = "4.4px 0px 11.3px 12.5px";
+            nav.style.borderTop = "1px solid #bdbdbd";
+        }
+        else {
+            nav.style.maxHeight = "0px"
+            nav.style.padding = "0px 0px 0px 12.5px";
+            nav.style.borderTop = "1px solid transparent";
+        }
+    })
+})
